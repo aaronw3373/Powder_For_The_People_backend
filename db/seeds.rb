4d89364872de2e10ef59a34790f4a180c44609c3
aaronw3373 <aaronw3373@gmail.com>
2015-06-05 19:21:00 -0400

    stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,71 +11,71 @@
 # Resort.destroy_all
 # User.destroy_all
 
-# Resort.create(name:'Loon', vertical: 2100, acres: 370, location:"pws:KNHLINCO2")
-# Resort.create(name:'Stowe', vertical: 2360, acres: 485, location:"pws:MMMNV1")
-# Resort.create(name:'Killington', vertical: 3050, acres: 1509, location:"pws:KVTKILLI2")
-# Resort.create(name:'Sunday River', vertical: 2350, acres: 870, location:"pws:KMENEWRY6")
-# Resort.create(name:'Mount Snow', vertical: 1700, acres: 588, location:"pws:MMSWV1")
-# Resort.create(name:'Sugarloaf', vertical: 2820, acres: 1056, location:"pws:KMECARRA3")
-# Resort.create(name:"Jay Peak", vertical:2153, acres:385, location:"pws:KVTMONTG3")
-# Resort.create(name:"Cannon",vertical:2180,acres:282, location:"pws:KNHFRANC6")
-# Resort.create(name:"Smugglers Notch", vertical:2610, acres:1000, location:"pws:MMMNV1")
-# Resort.create(name:"Sugarbush", vertical:2600, acres:578, location:"pws:KVTWARRE3")
-# Resort.create(name:"Mad River Glen", vertical:2037, acres:120, location:"pws:KVTSTARK2")
-# Resort.create(name:"Jackson Hole", vertical:4139, acres:2500, location:"pws:MJHR");
-# Resort.create(name:"Mount Sunapee", vertical:1510, acres:233, location:"pws:ME1526");
-# Resort.create(name:"Vail", vertical:3450, acres:5289, location:"pws:KCOVAIL6");
-# Resort.create(name:"Wildcat", vertical:2112, acres:225, location:"pws:KNHJACKS6");
-# Resort.create(name:"Okemo", vertical:2100, acres:632, location:"pws:KVTLUDLO1");
-# Resort.create(name:"Waterville Valley", vertical:2020, acres:259, location:"pws:MWVVN3");
-# Resort.create(name:"Stratton", vertical:2003, acres:625, location:"pws:KVTSTRAT6");
-# Resort.create(name:"Saddleback", vertical:2000, acres:220, location:"pws:KMERANGE10");
-# Resort.create(name:"Burke", vertical:2000, acres:260, location:"pws:MGALV1");
-# Resort.create(name:"Pico", vertical:1967, acres:468, location:"pws:KVTBRIDG2");
-# Resort.create(name:"Attitash", vertical:1750, acres:310, location:"pws:KNHGLEN2");
-# Resort.create(name:"Magic Moutain", vertical:1700, acres:195, location:"pws:KVTCHEST5");
-# Resort.create(name:"Bolton Valley", vertical:1625, acres:165, location:"pws:KVTBOLTO2");
-# Resort.create(name:"Bretton Woods", vertical:1500, acres:464, location:"pws:KNHHARTS3");
-# Resort.create(name:"Gunstock Mountain", vertical:1400, acres:227, location:"pws:KNHGILFO8");
-# Resort.create(name:"Black Mountain ME", vertical:1380, acres:300, location:"pws:MROXM1");
-# Resort.create(name:"Bromley", vertical:1334, acres:178, location:"pws:KVTVERMO8");
-# Resort.create(name:"Shawnee Peak", vertical:1300, acres:249, location:"pws:KMEBRIDG1");
-# Resort.create(name:"Ragged Mountain", vertical:1250, acres:220, location:"pws:KNHDANBU2");
-# Resort.create(name:"Cranmore Mountain", vertical:1200, acres:200, location:"pws:KNHCONWA12");
-# Resort.create(name:"Berkshire East", vertical:1180, acres:400, location:"pws:MCHAM3");
-# Resort.create(name:"Mount Abram", vertical:1150, acres:560, location:"pws:KMEBETHE2");
-# Resort.create(name:"Jiminy Peak", vertical:1140, acres:170, location:"pws:KMALANES3");
-# Resort.create(name:"Ski Butternut", vertical:1119, acres:110, location:"pws:KMAGREAT4");
-# Resort.create(name:"Black Mountain NH", vertical:1110, acres:143, location:"pws:KNHJACKS6");
-# Resort.create(name:"Middlebury College Snow Bowl", vertical:1040, acres:110, location:"pws:KVTRIPTO2");
-# Resort.create(name:"Crotched Mountain", vertical:1016, acres:100, location:"pws:KNHFRANC4");
-# Resort.create(name:"Wachusett Mountain", vertical:1000, acres:110, location:"pws:KMAHUBBA2");
-# Resort.create(name:"Catamount", vertical:1000, acres:119, location:"pws:KMAEGREM2");
-# Resort.create(name:"Bigrock", vertical:980, acres:60, location:"pws:KMEMONTI1");
-# Resort.create(name:"Dartmouth Skiway", vertical:968, acres:107, location:"pws:KNHLYME7");
-# Resort.create(name:"Camden Snow Bowl", vertical:943, acres:60, location:"pws:MWRKM1");
-# Resort.create(name:"Bousquet", vertical:750, acres:200, location:"pws:KMAPITTS15");
-# Resort.create(name:"Pats Peak", vertical:710, acres:90, location:"pws:KNHHENNI4");
-# Resort.create(name:"Whaleback", vertical:700, acres:85, location:"pws:KNHENFIE2");
-# Resort.create(name:"Mohawk Mountain", vertical:650, acres:107, location:"pws:KCTLITCH6");
-# Resort.create(name:"Suicide Six", vertical:650, acres:107, location:"pws:KVTSOUTH2");
-# Resort.create(name:"Ski Sundown", vertical:625, acres:65, location:"pws:KCTCANTO3");
-# Resort.create(name:"Granite Gorge", vertical:525, acres:25, location:"pws:MOTRN3");
-# Resort.create(name:"Cochran's Ski Area", vertical:500, acres:30, location:"pws:KVTRICHM6");
-# Resort.create(name:"Blandford", vertical:465, acres:158, location:"pws:KMAHAMPD3");
-# Resort.create(name:"Mount Southington", vertical:425, acres:51, location:"pws:KCTSOUTH41");
-# Resort.create(name:"Otis Ridge", vertical:400, acres:60, location:"pws:KMAMONTE2");
-# Resort.create(name:"Titcomb Mountain", vertical:350, acres:45, location:"pws:KMEFARMI3");
-# Resort.create(name:"King Pine", vertical:350, acres:45, location:"pws:KNHEATON3");
-# Resort.create(name:"Blue Hills", vertical:309, acres:60, location:"pws:KMAMILTO3");
-# Resort.create(name:"Woodbury", vertical:300, acres:100, location:"pws:KCTWOODB7");
-# Resort.create(name:"Ski Bradford", vertical:248, acres:48, location:"pws:KMABRADF5");
-# Resort.create(name:"Yawgoo Valley", vertical:245, acres:36, location:"pws:KRINORTH19");
-# Resort.create(name:"Nashoba Valley", vertical:240, acres:59, location:"pws:KMALITTL2");
-# Resort.create(name:"Ski Ward", vertical:220, acres:45, location:"pws:KMASHREW17");
+Resort.create(name:'Loon', vertical: 2100, acres: 370, location:"pws:KNHLINCO2")
+Resort.create(name:'Stowe', vertical: 2360, acres: 485, location:"pws:MMMNV1")
+Resort.create(name:'Killington', vertical: 3050, acres: 1509, location:"pws:KVTKILLI2")
+Resort.create(name:'Sunday River', vertical: 2350, acres: 870, location:"pws:KMENEWRY6")
+Resort.create(name:'Mount Snow', vertical: 1700, acres: 588, location:"pws:MMSWV1")
+Resort.create(name:'Sugarloaf', vertical: 2820, acres: 1056, location:"pws:KMECARRA3")
+Resort.create(name:"Jay Peak", vertical:2153, acres:385, location:"pws:KVTMONTG3")
+Resort.create(name:"Cannon",vertical:2180,acres:282, location:"pws:KNHFRANC6")
+Resort.create(name:"Smugglers Notch", vertical:2610, acres:1000, location:"pws:MMMNV1")
+Resort.create(name:"Sugarbush", vertical:2600, acres:578, location:"pws:KVTWARRE3")
+Resort.create(name:"Mad River Glen", vertical:2037, acres:120, location:"pws:KVTSTARK2")
+Resort.create(name:"Jackson Hole", vertical:4139, acres:2500, location:"pws:MJHR");
+Resort.create(name:"Mount Sunapee", vertical:1510, acres:233, location:"pws:ME1526");
+Resort.create(name:"Vail", vertical:3450, acres:5289, location:"pws:KCOVAIL6");
+Resort.create(name:"Wildcat", vertical:2112, acres:225, location:"pws:KNHJACKS6");
+Resort.create(name:"Okemo", vertical:2100, acres:632, location:"pws:KVTLUDLO1");
+Resort.create(name:"Waterville Valley", vertical:2020, acres:259, location:"pws:MWVVN3");
+Resort.create(name:"Stratton", vertical:2003, acres:625, location:"pws:KVTSTRAT6");
+Resort.create(name:"Saddleback", vertical:2000, acres:220, location:"pws:KMERANGE10");
+Resort.create(name:"Burke", vertical:2000, acres:260, location:"pws:MGALV1");
+Resort.create(name:"Pico", vertical:1967, acres:468, location:"pws:KVTBRIDG2");
+Resort.create(name:"Attitash", vertical:1750, acres:310, location:"pws:KNHGLEN2");
+Resort.create(name:"Magic Moutain", vertical:1700, acres:195, location:"pws:KVTCHEST5");
+Resort.create(name:"Bolton Valley", vertical:1625, acres:165, location:"pws:KVTBOLTO2");
+Resort.create(name:"Bretton Woods", vertical:1500, acres:464, location:"pws:KNHHARTS3");
+Resort.create(name:"Gunstock Mountain", vertical:1400, acres:227, location:"pws:KNHGILFO8");
+Resort.create(name:"Black Mountain ME", vertical:1380, acres:300, location:"pws:MROXM1");
+Resort.create(name:"Bromley", vertical:1334, acres:178, location:"pws:KVTVERMO8");
+Resort.create(name:"Shawnee Peak", vertical:1300, acres:249, location:"pws:KMEBRIDG1");
+Resort.create(name:"Ragged Mountain", vertical:1250, acres:220, location:"pws:KNHDANBU2");
+Resort.create(name:"Cranmore Mountain", vertical:1200, acres:200, location:"pws:KNHCONWA12");
+Resort.create(name:"Berkshire East", vertical:1180, acres:400, location:"pws:MCHAM3");
+Resort.create(name:"Mount Abram", vertical:1150, acres:560, location:"pws:KMEBETHE2");
+Resort.create(name:"Jiminy Peak", vertical:1140, acres:170, location:"pws:KMALANES3");
+Resort.create(name:"Ski Butternut", vertical:1119, acres:110, location:"pws:KMAGREAT4");
+Resort.create(name:"Black Mountain NH", vertical:1110, acres:143, location:"pws:KNHJACKS6");
+Resort.create(name:"Middlebury College Snow Bowl", vertical:1040, acres:110, location:"pws:KVTRIPTO2");
+Resort.create(name:"Crotched Mountain", vertical:1016, acres:100, location:"pws:KNHFRANC4");
+Resort.create(name:"Wachusett Mountain", vertical:1000, acres:110, location:"pws:KMAHUBBA2");
+Resort.create(name:"Catamount", vertical:1000, acres:119, location:"pws:KMAEGREM2");
+Resort.create(name:"Bigrock", vertical:980, acres:60, location:"pws:KMEMONTI1");
+Resort.create(name:"Dartmouth Skiway", vertical:968, acres:107, location:"pws:KNHLYME7");
+Resort.create(name:"Camden Snow Bowl", vertical:943, acres:60, location:"pws:MWRKM1");
+Resort.create(name:"Bousquet", vertical:750, acres:200, location:"pws:KMAPITTS15");
+Resort.create(name:"Pats Peak", vertical:710, acres:90, location:"pws:KNHHENNI4");
+Resort.create(name:"Whaleback", vertical:700, acres:85, location:"pws:KNHENFIE2");
+Resort.create(name:"Mohawk Mountain", vertical:650, acres:107, location:"pws:KCTLITCH6");
+Resort.create(name:"Suicide Six", vertical:650, acres:107, location:"pws:KVTSOUTH2");
+Resort.create(name:"Ski Sundown", vertical:625, acres:65, location:"pws:KCTCANTO3");
+Resort.create(name:"Granite Gorge", vertical:525, acres:25, location:"pws:MOTRN3");
+Resort.create(name:"Cochran's Ski Area", vertical:500, acres:30, location:"pws:KVTRICHM6");
+Resort.create(name:"Blandford", vertical:465, acres:158, location:"pws:KMAHAMPD3");
+Resort.create(name:"Mount Southington", vertical:425, acres:51, location:"pws:KCTSOUTH41");
+Resort.create(name:"Otis Ridge", vertical:400, acres:60, location:"pws:KMAMONTE2");
+Resort.create(name:"Titcomb Mountain", vertical:350, acres:45, location:"pws:KMEFARMI3");
+Resort.create(name:"King Pine", vertical:350, acres:45, location:"pws:KNHEATON3");
+Resort.create(name:"Blue Hills", vertical:309, acres:60, location:"pws:KMAMILTO3");
+Resort.create(name:"Woodbury", vertical:300, acres:100, location:"pws:KCTWOODB7");
+Resort.create(name:"Ski Bradford", vertical:248, acres:48, location:"pws:KMABRADF5");
+Resort.create(name:"Yawgoo Valley", vertical:245, acres:36, location:"pws:KRINORTH19");
+Resort.create(name:"Nashoba Valley", vertical:240, acres:59, location:"pws:KMALITTL2");
+Resort.create(name:"Ski Ward", vertical:220, acres:45, location:"pws:KMASHREW17");
 
 
-# puts "CREATED FIRST SET 61 RESORTS"
+puts "CREATED FIRST SET 61 RESORTS"
 
 
 # #Maryland and new jersey
@@ -151,7 +151,7 @@
 
 
 
-# User.create!(name:"Aaron",email:"aaronw3373@gmail.com", privileges:"god", password: 'password1')
+User.create!(name:"Aaron",email:"aaronw3373@gmail.com", privileges:"god", password: 'password1')
 
 
 # puts "CREATED 1 USER"
